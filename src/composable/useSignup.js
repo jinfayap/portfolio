338ca564a04c27ddc235400c18373832f9718ab5
jinfayap <jinfayap@gmail.com>
2020-12-28 18:48:47 +0800

    Add signup composable

---
+++ src/composable/useSignup.js
@@ -0,0 +1,29 @@
+import {projectAuth} from '../firebase/config'
+import {ref} from 'vue'
+
+const error = ref(null)
+const isPending = ref(false)
+
+const signUp = async (email, password, displayName) => {
+    isPending.value = true
+    error.value = null
+    try {
+        const res = await projectAuth.createUserWithEmailAndPassword(email, password)
+        if(!res) {
+            throw new Error('Could not complete the signup')
+        }
+        console.log(res)
+        await res.user.updateProfile({displayName})
+        isPending.value = false
+        return res
+    } catch (err) {
+        error.value = err.message
+        isPending.value = false
+    }  
+}
+
+const useSignup = () => {
+    return {signUp, error, isPending}
+}
+
+export default useSignup
