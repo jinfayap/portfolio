2bab3d801c19c2431c0b6f900f5d2a50ea98aa74
jinfayap <jinfayap@gmail.com>
2021-01-04 18:26:31 +0800

    Shift to portfolio folder

---
+++ src/views/portfolio/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    Portfolio
+    <About />
+  </div>
+</template>
+
+<script>
+import About from '@/components/About.vue'
+export default {
+  name: 'Home',
+  components: {About}
+}
+</script>
