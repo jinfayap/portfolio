6aae41477a9db18f00ae9f583732c3d6f72991c0
jinfayap <jinfayap@gmail.com>
2021-01-06 18:24:35 +0800

    Remove console.log

--- src/composable/useSignup.js
+++ src/composable/useSignup.js
@@ -12,9 +12,9 @@ const signUp = async (email, password, displayName) => {
         if(!res) {
             throw new Error('Could not complete the signup')
         }
-        console.log(res)
         await res.user.updateProfile({displayName})
         isPending.value = false
+        error.value = null
         return res
     } catch (err) {
         error.value = err.message
