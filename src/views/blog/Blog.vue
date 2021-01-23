74aab6f743548a2e7c14523281c7fdfb19f71470
jinfayap <jinfayap@gmail.com>
2021-01-23 21:55:16 +0800

    Finish blog content

--- src/views/blog/Blog.vue
+++ src/views/blog/Blog.vue
@@ -2,13 +2,17 @@
   <div></div>
   <div class = 'blog-box'>
       <div class="blog-content">
-          blog content
           <!-- a single blog component -->
+          <div v-if = 'blogs' class = 'blog-container'>
+              <div v-for = 'blog in blogs' :key = 'blog.id' class = 'blog-container'>
+                  <SingleBlog :blog = blog />
+              </div>
+          </div>
       </div>
       <div class="blog-sidebar">
           blog sidebar
           <!-- Add a blog button here -->
-          <router-link to = '#'>
+          <router-link :to = "{ name: 'AddNewBlog' }">
               <button>Add a new Blog Post</button>
           </router-link>
           <!-- Cloud tag -->
@@ -17,8 +21,15 @@
 </template>
 
 <script>
-export default {
+import getCollection from '@/composable/getCollection'
+import SingleBlog from '@/components/blog/SingleBlog.vue'
 
+export default {
+    components: { SingleBlog },
+    setup() {
+        const { documents:blogs, error } = getCollection('blogs')
+        return { blogs, error }
+    }
 }
 </script>
 
@@ -29,16 +40,17 @@ export default {
     gap: 10px;
 }
 .blog-content {
-    border: 1px solid black;
     display: flex;
     flex-direction: column;
-    align-items: center
+    align-items: center;
 }
 .blog-sidebar {
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
     align-items: center;
     border: 1px solid red;
+}
+.blog-container {
+    width: 100%;
 }
 </style>
