6b2307ea8c3dc4aa0323ebb7e78d97f3e57f94e2
jinfayap <jinfayap@gmail.com>
2021-01-23 19:35:40 +0800

    Real time listener composable

---
+++ src/composable/getCollection.js
@@ -0,0 +1,35 @@
+import { ref, watchEffect } from 'vue'
+import { projectFirestore } from '../firebase/config'
+
+const error = ref(null)
+const documents = ref(null)
+
+const useCollection = (collection) => {
+    error.value = null
+
+    let collectionRef = projectFirestore.collection(collection).orderBy('createdAt', 'desc')
+
+    const unsub = collectionRef.onSnapshot(snapshot => {
+        let results = []
+        console.log(snapshot)
+        snapshot.docs.forEach(doc => {
+             doc.data().createdAt && results.push({...doc.data(), id: doc.id})
+        })
+
+        documents.value = results
+        error.value = null
+    }, err => {
+        documents.value = null
+        error.value = 'Could not fetch the data'
+    })
+
+    watchEffect(onInvalidate => {
+        onInvalidate(() => {
+            unsub()
+        })
+    })
+
+    return { error, documents }
+}
+
+export default useCollection
