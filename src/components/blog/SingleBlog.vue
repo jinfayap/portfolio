3c7e5f98ba7e5dd83f5495237d4002bcac4c7472
jinfayap <jinfayap@gmail.com>
2021-01-23 21:54:48 +0800

    Flesh out details in single blog

---
+++ src/components/blog/SingleBlog.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class = 'singleblog-box'>
+        <h2 class = 'blog-title'> {{blog.title}} </h2>
+        <div class = 'blog-description' v-html = 'blog.description'></div>
+        <div v-if = 'blog.imageUrl' class = 'blog-image-box'>
+            <img :src="blog.imageUrl">
+        </div>
+      <div class = 'tag-box blog-tag-box'>
+          Current tags:
+          <div v-for = 'tagname in blog.tagList' :key = tagname >
+            <span class = 'tagpill' @click = 'handleClick(tagname)'>{{tagname}}</span>
+         </div>
+      </div>
+      <div class = 'time-created'>
+          Post created - {{ longAgo }} ago
+      </div>
+    </div>
+
+</template>
+
+<script>
+import { formatDistanceToNow } from 'date-fns'
+import { computed } from 'vue'
+export default {
+    props: [ 'blog' ],
+    setup(props) {
+        const longAgo = computed(() => {
+            return formatDistanceToNow(new Date(props.blog.createdAt.seconds *1000))
+        })
+        return { longAgo }
+    }
+}
+</script>
+
+<style>
+.singleblog-box {
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+    margin: 0 auto;
+    margin-bottom: 20px;
+    align-items: center;
+    border-radius: 10px;
+    position: relative;
+}
+.blog-title {
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    font-weight: 600;
+    font-size: 20px;
+    margin-top: 5px;
+}
+.blog-image-box img {
+    width: 100%;
+    max-height: 180px;
+}
+.blog-description, 
+.blog-image-box,
+.blog-tag-box {
+    padding: 20px;
+    width: 100%;
+}
+.tag-box.blog-tag-box .tagpill {
+    cursor: none;
+    font-size: 12px;
+    padding: 5px;
+}
+.tag-box.blog-tag-box .tagpill:hover {
+    background-color: cyan;
+}
+.time-created {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    font-size: 10px;
+    font-weight: 400;
+}
+</style>
