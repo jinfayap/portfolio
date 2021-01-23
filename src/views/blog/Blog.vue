4c788ff24649f6be97f027a16d882c7281dad091
jinfayap <jinfayap@gmail.com>
2021-01-23 10:33:37 +0800

    Draft basic layout

--- src/views/blog/Blog.vue
+++ src/views/blog/Blog.vue
@@ -1,5 +1,19 @@
 <template>
-  Blog home page
+  <div></div>
+  <div class = 'blog-box'>
+      <div class="blog-content">
+          blog content
+          <!-- a single blog component -->
+      </div>
+      <div class="blog-sidebar">
+          blog sidebar
+          <!-- Add a blog button here -->
+          <router-link to = '#'>
+              <button>Add a new Blog Post</button>
+          </router-link>
+          <!-- Cloud tag -->
+      </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +23,22 @@ export default {
 </script>
 
 <style>
-
+.blog-box {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    gap: 10px;
+}
+.blog-content {
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    align-items: center
+}
+.blog-sidebar {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid red;
+}
 </style>
